2479c9af379b3120aff45bf68a2e80b2f5a9aaae
SergeyK92 <sergey.korshak.92@mail.ru>
2020-02-25 23:47:36 +0300

    Commit #2

---
+++ homeWork_02/ex3_ArithmeticOperations/Sergey_Korshak/java/src/by/korshak/sergey/App.java
@@ -0,0 +1,7 @@
+package by.korshak.sergey;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Проверочка работы идеи с ГИТ");
+    }
+}
---
+++ homeWork_02/ex3_ArithmeticOperations/Sergey_Korshak/java/src/by/korshak/sergey/dog.java
@@ -0,0 +1,7 @@
+package by.korshak.sergey;
+
+public class dog {
+    public static void main(String[] args) {
+        System.out.println("проверка №2");
+    }
+}
