d06b292282ac2ab2313733fc7a3ee3b86184fade
SergeyK92 <sergey.korshak.92@mail.ru>
2020-02-25 23:58:51 +0300

    Commit #3

--- homeWork_02/ex3_ArithmeticOperations/Sergey_Korshak/java/src/by/korshak/sergey/dog.java
+++ homeWork_02/ex3_ArithmeticOperations/Sergey_Korshak/java/src/by/korshak/sergey/dog.java
@@ -2,6 +2,21 @@ package by.korshak.sergey;
 
 public class dog {
     public static void main(String[] args) {
-        System.out.println("проверка №2");
-    }
+        System.out.println("проверка №2222222");
+        int a = 2;
+        int b = 3;
+        float c = a * (float)b;
+        System.out.println(c);
+
+
+
+
+   }
+
+    int a = 2;
+    int b = 3;
+    float c = a * (float)b;
+
 }
+
+
